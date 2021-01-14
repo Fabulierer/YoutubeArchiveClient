8fe5ce3a31514d0c6ba2c565dae75217f97a9499
fabius <fabius.drouven@protonmail.com>
2021-01-14 12:25:21 +0100

    finished "addvideo" dialog

--- src/ProgressGUI.java
+++ src/ProgressGUI.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.net.URL;
 
@@ -10,13 +12,24 @@ public class ProgressGUI {
     private JLabel labelTitle;
 
     private JLabel[] jobsIcon;
+    private GridBagConstraints c;
 
     private final ImageIcon ICON_SUCCESS = new ImageIcon("./res/success.png");
     private final ImageIcon ICON_ERROR = new ImageIcon("./res/error.png");
     private final ImageIcon ICON_LOADING = new ImageIcon("./res/loading.gif");
 
     public ProgressGUI(String title, String[] jobs) {
-        GridBagConstraints c = new GridBagConstraints();
+        JFrame frame = new JFrame("Progress");
+        frame.setContentPane(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        c = new GridBagConstraints();
         jobsIcon = new JLabel[jobs.length];
         labelTitle.setText("<html><span style='font-size:40px'> "+ title + "</span></html>");
         c.gridy = 0;
@@ -28,19 +41,17 @@ public class ProgressGUI {
             panelInfo.add(new JLabel("<html><span style='font-size:20px'> "+ jobs[i] + "</span></html>"), c);
             c.gridy++;
         }
+        buttonCancel.addActionListener(e -> frame.dispose());
+        frame.setVisible(true);
     }
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Progress");
-        frame.setContentPane(new ProgressGUI("Test", new String[]{"this", "is", "a", "test"}).panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        }
-        frame.setVisible(true);
+    public void setDone(int i) {
+        jobsIcon[i].setIcon(ICON_SUCCESS);
+    }
+
+    public void setError(int i, String msg) {
+        jobsIcon[i].setIcon(ICON_ERROR);
+        JOptionPane.showMessageDialog(panel, msg, "Error", JOptionPane.ERROR_MESSAGE);
     }
 
 }
